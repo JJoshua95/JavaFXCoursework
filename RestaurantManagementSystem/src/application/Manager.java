bf3e1d2a82f6c001942d1040e5fe135ce744bb01
JJoshua95 <jcjoshua@Jarrods-MacBook-Pro.local>
2016-12-02 20:08:40 +0000

    JavaFX Coursework in progress
    First Commit

---
+++ RestaurantManagementSystem/src/application/Manager.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Manager extends Employee {
+	
+	private final boolean isManager = true;
+	
+	public Manager(String username, String password) {
+		super(username, password);
+	}
+
+}
