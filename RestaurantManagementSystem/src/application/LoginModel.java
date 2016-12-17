329853c82081e733f12eea8299a88696b8dedafa
JJoshua95 <jcjoshua@Jarrods-MacBook-Pro.local>
2016-12-17 20:25:50 +0000

    Restructuring and commenting

--- RestaurantManagementSystem/src/application/LoginModel.java
+++ RestaurantManagementSystem/src/application/LoginModel.java
@@ -1,11 +1,25 @@
 package application;
 
-import java.sql.*;
+import java.sql.*; 
+
+/**
+ * The LoginModel class contains all the methods that the LoginScreenController class uses to access and interact with the database, 
+ * this class focuses on verifying the login details a potential user may enter, it also initialises the necessary tables that this 
+ * application will need to function at the bare minimum if the local RestaurantDB.sqlite file is deleted and a new one is created 
+ * with no tables by default. There is also a special emergency admin account inserted into the database to allow access back in if the 
+ * database is deleted.
+ * @author Jarrod Joshua
+ */
 
 public class LoginModel {
 	
 	private Connection connection;
 	
+	/**
+	 * The LoginModel class constructor initialises a connection to the SQLite database and initialises tables if they are not found
+	 * The methods and structure in this class and its children are derived from the tutorials available from the ProgrammingKnowledge playlist 
+	 * JavaFX tutorial for beginners : https://www.youtube.com/watch?v=9YrmON6nlEw&list=PLS1QulWo1RIaUGP446_pWLgTZPiFizEMq
+	 */
 	LoginModel() {
 		connection = SqliteConnection.Connector();
 		if (connection == null) {
@@ -21,6 +35,10 @@ public class LoginModel {
 		initActivityLogTable() ;
 	}
 	
+	/**
+	 * Determines if the database is connected.
+	 * @return boolean : true if the database is connected, false is there is no connection found.
+	 */
 	boolean isDbConnected() {
 		try {
 			return !connection.isClosed();
@@ -36,7 +54,7 @@ public class LoginModel {
 		ResultSet resultSet = null;
 		//String query = "select * from staff where username = ? and password = ?"; 
 		String query = "select * from staff where username = ? and password = ? and isManager = 'false'";
-		// query database for username and password from user and password args and for false isManager
+		// query database for username and password from user and password arguments and for false isManager
 		try {
 			preparedStatement = connection.prepareStatement(query);
 			
